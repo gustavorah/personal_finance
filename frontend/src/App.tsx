ee0bd8ebe2b1627c53955235f70da48aa281ab07
Gustavo Rahmeier <90331005+gustavorah@users.noreply.github.com>
2023-11-07 11:22:51 -0300

    initial frontend configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Pairs"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/ListGroup.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+  const [selectedIndex, setSelectIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectIndex(index);
+              onSelectItem(item);
+            }}
+            key={item}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
